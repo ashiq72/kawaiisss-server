2d88bfe3b45f868f4a3856479ae6e4b9c7e9294b
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-11-30 10:50:51 +0600

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-const express = require("express");
-const app = express();
-var cors = require("cors");
 const port = 5000;
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("Hello World! About page");
-});
+const app = require("./app");
 
 app.listen(port, () => {
   console.log(`Server link ${port}`);
