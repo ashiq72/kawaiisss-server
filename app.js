b23ffbb7b8257a16812574a1e7cc0f4743ea535b
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-11-30 10:52:08 +0600

    first commit

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
+app.get("/about", (req, res) => {
+  res.send("About is working!");
+});
 
 module.exports = app;
